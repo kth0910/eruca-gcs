9fe76ab99d4a36dd46f2dd5b89f475bb021d411f
kth0910 <kth_04@naver.com>
2025-07-14 20:05:53 +0900

    인코딩 순서 수정

--- rpi/command_sender.py
+++ rpi/command_sender.py
@@ -1,12 +1,9 @@
 # rpi/command_sender.py
 
 import serial
-import json
 
 def send_command(ser: serial.Serial, cmd: str):
-    packet = {
-        "type": "command",
-        "cmd": cmd
-    }
-    ser.write("EJECT" + "\n").encode('utf-8')
+    line = f"{cmd}\n"   
+    ser.write(line.encode())   # 먼저 encode → bytes
+    ser.flush()
     print("[CMD] Sent:", cmd)
